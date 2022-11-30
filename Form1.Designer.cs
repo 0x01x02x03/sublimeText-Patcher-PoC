480585725584688ce95ef2f476a0b1109cdbcd4f
emilio filipigh lopez cano <emiliofilipigh@gmail.com>
2022-11-29 21:35:54 -0300

    PoC SublimeText Patcher

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace sublimeText_autoPatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnFile);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select SublimeText.exe with the below button menu";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(13, 174);
+            this.txtKey.Multiline = true;
+            this.txtKey.Name = "txtKey";
+            this.txtKey.ReadOnly = true;
+            this.txtKey.Size = new System.Drawing.Size(331, 177);
+            this.txtKey.TabIndex = 2;
+            this.txtKey.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(101, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Use the License Key below";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(77, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = " to get your SublimeText Full version";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(150, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "- Step 2 -";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(150, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "- Step 1 -";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(73, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(211, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "==================================";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 358);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "- PoC - sublimeText autoPatcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sublimeText_autoPatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFile(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile= new OpenFileDialog();
+            openFile.DefaultExt = "*.exe";
+            openFile.ShowDialog();
+            if (openFile.CheckFileExists)
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.Open(openFile.FileName, FileMode.Open, FileAccess.ReadWrite)))
+                {
+                    int offset = 5987213; //position you want to start editing
+                    byte[] new_data = new byte[] { 0x33, 0xC0, 0xFE, 0xC0, 0xC3, 0x57, 0x55, 0x53, 0xB8, 0x28, 0x21, 0x00, 0x00 }; //new data
+                    writer.Seek(offset, SeekOrigin.Begin); //move your cursor to the position
+                    //String memoria = writer.Seek(offset, SeekOrigin.Begin).ToString();
+                    //MessageBox.Show(memoria);
+                    writer.Write(new_data);//write it
+                    int offsetDos = 7821178;
+                    byte[] new_dataDos = new byte[] { 0x73, 0x75, 0x62, 0x6C, 0x69, 0x6D, 0x65, 0x68, 0x71, 0x2E, 0x6C, 0x6F, 0x63, 0x61, 0x6C, 0x68, 0x6F, 0x73, 0x74, 0x00, 0x00 }; //new data
+                    writer.Seek(offsetDos, SeekOrigin.Begin); //move your cursor to the position
+                    //String memoria = writer.Seek(offset, SeekOrigin.Begin).ToString();
+                    writer.Write(new_dataDos);//write it
+
+                    MessageBox.Show("Step 1 completed, now follow step 2");
+                    txtKey.Text = "----- BEGIN LICENSE -----\r\nCyberTitus\r\nUnlimited User License\r\nEA7E-81044230\r\n0C0CD4A8 CAA317D9 CCABD1AC 434C984C\r\n7E4A0B13 77893C3E DD0A5BA1 B2EB721C\r\n4BAAB4C4 9B96437D 14EB743E 7DB55D9C\r\n7CA26EE2 67C3B4EC 29B2C65A 88D90C59\r\nCB6CCBA5 7DE6177B C02C2826 8C9A21B0\r\n6AB1A5B6 20B09EA2 01C979BD 29670B19\r\n92DC6D90 6E365849 4AB84739 5B4C3EA1\r\n048CC1D0 9748ED54 CAC9D585 90CAD815\r\n------ END LICENSE ------";
+                }
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
